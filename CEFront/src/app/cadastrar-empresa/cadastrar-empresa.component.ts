f0d7aa24a7bae7256e5d86749423292403bbd278
Alvaro Augusto Pereira <alvaro.pereira@ibm.com>
2022-07-10 11:48:07 -0300

    cadastrar empresa page

--- CEFront/src/app/cadastrar-empresa/cadastrar-empresa.component.ts
+++ CEFront/src/app/cadastrar-empresa/cadastrar-empresa.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-cadastrar-empresa',
@@ -7,9 +14,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarEmpresaComponent implements OnInit {
 
-  constructor() { }
+  formulario!: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder,
+    // private service: CademService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      cnpj: [''],
+      razaoSocial: [''],
+      nomeFantasia: [''],
+      apelido: [''],
+      telefone: [''],
+      email: [''],
+      cnaeSecao: [''],
+      cnaeClasse: [''],
+      // cnaeSecao: [''],
+      // cnaeSecao: [''],
+      // cnaeSecao: [''],      
+    });
+  }
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
+  cadastrar(): void{
+    console.log(this.formulario.value)
   }
 
 }
+
+
--- CEFront/src/app/cadastrar-empresa/cadastrar-empresa.module.ts
+++ CEFront/src/app/cadastrar-empresa/cadastrar-empresa.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
-import { FormControl, FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 import { CadastrarEmpresaComponent } from './cadastrar-empresa.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -16,8 +20,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     CommonModule,
     MatCardModule,     
     FormsModule,
+    ReactiveFormsModule,
     MatInputModule,
-    MatFormFieldModule   
+    MatFormFieldModule, 
+    MatSelectModule,
+    MatButtonModule
   ]
 })
 export class CadastrarEmpresaModule { }
